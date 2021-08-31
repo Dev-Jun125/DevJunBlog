61a5a10b7d1ee09c49a30a48875f0b3144a1f311
DongJun Kim <dev.jun125@gmail.com>
2021-08-31 19:09:46 +0900

    Add models(동적사이트)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,22 @@
+from django.http.response import HttpResponseRedirect
+from accountapp.models import HelloWorld
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render, resolve_url
 
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+        
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(resolve_url('accountapp:hello_world'))
+        
+    else:
+        hello_world_list = HelloWorld.objects.all()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
